a0e637d6efee17140b8c126d965e78ce8d067632
Arseniy2059 <semyashulkovy64@mail.ru>
2024-08-28 19:10:54 +0300

    Add files via upload

---
+++ нол.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+index = 0
+while index < len(my_list):
+    element = my_list[index]
+    index += 1
+    if element < 0:
+        break
+    elif element == 0:
+        continue
+    else:
+        print(element)
